4ae1c200c8aaef79d984bff53834675b50e7ad8b
Jack Blank <81026825+GitHubUser1024@users.noreply.github.com>
2021-05-15 19:39:58 +0800

    Create GitHub.h

---
+++ GitHub.h
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdio.h>
+#include<windows.h>
+#include<time.h>
+#include<ctime>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<assert.h>
+#include<ctype.h>
+#include<float.h>
+#include<limits.h>
+#include<cstring>
+#include<fstream>
+#include<string>
+#include <TlHelp32.h>
+#include <Shlwapi.h>
+#include <Psapi.h>
+#include<bits/stdc++.h>
