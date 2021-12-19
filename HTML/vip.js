0c7d6cc2507e7ccdc2d42552e41b26b7da5ae8fc
Jack Blank <81026825+GitHubUser1024@users.noreply.github.com>
2021-12-19 16:22:01 +0800

    Update vip.js

--- HTML/vip.js
+++ HTML/vip.js
@@ -1,16 +1,11 @@
 jks = [
-
-    { url: "https://z1.m1907.cn/?jx=", title: "纯净接口:1907" },
-
     { url: "https://vip.parwix.com:4433/player/?url=", title: "B站解析:parwix" },
 
     { url: "https://okjx.cc/?url=", title: "接口:OK" },    
 
-    { title: "小蒋极致云", url: "https://www.kpezp.cn/jlexi.php?url=" },
-
     { url: "https://www.cuan.la/m3u8.php?url=", title: "③号接口-B站专用接口" },
    
-   { url: "https://m2090.com/?url=", title: "M2090" },
+    { url: "https://m2090.com/?url=", title: "M2090" },
 
-   { url: "https://www.mtosz.com/m3u8.php?url=", title: "Mao解析" },
+    { url: "https://www.mtosz.com/m3u8.php?url=", title: "Mao解析" },
 ];
