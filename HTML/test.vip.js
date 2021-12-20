0750e69c3ddabf5fafd077abaca52920477a5627
Jack Blank <81026825+GitHubUser1024@users.noreply.github.com>
2021-12-20 20:10:48 +0800

    Update test.vip.js

--- HTML/test.vip.js
+++ HTML/test.vip.js
@@ -1,3 +1,4 @@
 jks = [
-    { url: "https://www.mtosz.com/m3u8.php?url=", title: "接口：测试接口" },
+    { url: "https://www.mtosz.com/m3u8.php?url=", title: "接口：#43号测试接口" },
+    { url: "https://www.mtosz.com/m3u8.php?url=", title: "接口：#25号测试接口" },
 ];
